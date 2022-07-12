61ceafa573ef2e10b59eed83d6873c380ac8e50d
Guillian <guillian.brillard@epitech.eu>
2022-07-12 18:35:00 +0200

    deploy

---
+++ src/data.js
@@ -0,0 +1,55 @@
+export const projects = [
+    {
+      title: "SnapChat",
+      subtitle: "React Native et Firebase",
+      description:
+        "Reproduction d'un SnapChat like avec fonctionnalitées d'envoye et récéption de snaps",
+      image: "../assets/github.png",
+      link: "https://github.com/EpitechWebAcademiePromo2023/W-JSC-502-LYN-2-1-snapchat-guillian.brillard",
+    },
+    {
+      title: "Spotify",
+      subtitle: "ReactJS",
+      description:
+        "Reproduction d'un Spotify like avec fonction de recherche de titre par keyword dans une base de données",
+      image: "../assets/github.png",
+      link: "https://github.com/EpitechWebAcademiePromo2023/W-WEB-090-LYN-1-1-spotify-charlotte.renault",
+    },
+    {
+      title: "FrameWorkCSS",
+      subtitle: "SCSS / JS",
+      description:
+        "Création d'un Framework CSS type Bootstraps avec exemple visuel sur une blank page",
+      image: "../assets/github.png",
+      link: "https://github.com/EpitechWebAcademiePromo2023/W-INT-501-LYN-1-1-frameworkcss-guillian.brillard",
+    },
+    {
+      title: "My_Irc Relay Chat",
+      subtitle: "NodeJS / Socket.io / Express.js / React JS",
+      description:
+        "Création d'un chat en ligne avec système de gestion d'utilisateurs et de channels",
+      image: "../assets/github.png",
+      link: "https://github.com/EpitechWebAcademiePromo2023/W-JSC-502-LYN-2-1-irc-guillian.brillard",
+    },
+  ];
+  
+  export const skills = [
+    {
+      skill: "HTML / CSS",
+    },
+    {
+      skill: "JavaScript",
+    },
+    {
+      skill: "React JS",
+    },
+    {
+      skill: "React-Native",
+    },
+    {
+      skill: "PHP / MySQL",
+    },
+    {
+      skill: "TailWind / Bootstraps",
+    },
+  ];
